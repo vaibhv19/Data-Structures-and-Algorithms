a0b2d9b96d2c93326049f5d8051f6169644c5951
Your Name <vaibhavkrgupta369@gmail.com>
2026-07-01 15:28:27 +0530

    delete

--- src/Phase2_Patterns/x16_BitManipulation/Basics/BitBasics.java
+++
@@ -1,8 +0,0 @@
-﻿package Phase2_Patterns.x16_BitManipulation.Basics;
-
-public class BitBasics {
-    public static void main(String[] args) {
-        int x = 13;
-        System.out.println("Number of 1 bits in " + x + ": " + Integer.bitCount(x));
-    }
-}
--- src/Phase2_Patterns/x16_BitManipulation/BitMasking/BitMaskingExample.java
+++
@@ -1,8 +0,0 @@
-﻿package Phase2_Patterns.x16_BitManipulation.BitMasking;
-
-public class BitMaskingExample {
-    public static void main(String[] args) {
-        int mask = 5; // 101
-        System.out.println("Mask bits: " + ((mask & 1) != 0));
-    }
-}
